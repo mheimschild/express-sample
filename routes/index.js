7cd729b8d6167db418e56803e4450a3ed9bb7a86
Milan Heimschild <milan@heimschild.com>
2013-03-22 07:41:07 +0100

    mongo

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-var os = require('os');
+var os = require('os')
+  , mongo = require('mongoskin')
+  , client = mongo.db('http://192.168.33.10:27017/test', {w: 1})
+  , tags = client.collection('tags');
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express on ' + os.hostname() });
+exports.index = function(req, res) {
+  tags.count(function(err, count) {
+    if (err) throw err;
+    res.render('index', { title: 'Express on ' + os.hostname() + " " + count});
+  });
 };
